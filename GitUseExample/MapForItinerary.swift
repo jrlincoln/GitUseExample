4914ed4e745317e0403f9ff72e0627b76467239e
Jacob Lincoln <jake@jakelincoln.com>
2018-06-12 10:48:00 -0500

    Add new map-for-itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Jake on 6/12/18.
+//  Copyright © 2018 LyncoProducts. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
